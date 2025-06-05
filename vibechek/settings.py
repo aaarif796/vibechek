4684f5d41af4ce5f05f0cfd4802d9142b33a0480
aaarif796 <48153674+aaarif796@users.noreply.github.com>
2025-06-05 13:15:09 +0545

    Update settings.py

--- vibechek/settings.py
+++ vibechek/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!@05(@7aumywch1jonshmnpx-!(k8@gdogn39$&v^mgkc6*qnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vibechek.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
